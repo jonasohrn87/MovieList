701045231ab55a819b1740b31cf78703d6d2b510
Johodah <johnny.dahle@hotmail.com>
2024-11-01 10:45:16 +0100

    review is independent and responsive to the site.

--- movielist-react/src/components/Movie.jsx
+++ movielist-react/src/components/Movie.jsx
@@ -8,54 +8,57 @@ const Movie = () => {
     useContext(MovieContext);
 
   return (
-    <div> 
-    <div className="movies-grid">
+    <div>
+      <div className="movies-grid">
         {selectedMovie ? (
           <div>
-        <div key={selectedMovie.id} className="movie-card">
-          <h2>{selectedMovie.title}</h2>
-          {selectedMovie.poster && (
-            <img
-              src={`http://localhost:1337${selectedMovie.poster.url}`}
-              alt={`${selectedMovie.title} poster`}
-              className="movie-poster"
-            />
-          )}
-          <p className="movie-description">{selectedMovie.description}</p>
-          </div>
-          <div className="movie-review">
-              Betyg: {selectedMovie.rating}
-              <br />
-              {selectedMovie.review}
+            <div key={selectedMovie.id} className="movie-card">
+              <h2>{selectedMovie.title}</h2>
+              {selectedMovie.poster && (
+                <img
+                  src={`http://localhost:1337${selectedMovie.poster.url}`}
+                  alt={`${selectedMovie.title} poster`}
+                  className="movie-poster"
+                />
+              )}
+              <p className="movie-description">{selectedMovie.description}</p>
             </div>
           </div>
-      ) : (
-        searchFilter.map((movie) => (
-          <div key={movie.id} className="movie-card">
-            <h2>{movie.title}</h2>
-            {movie.poster && (
-              <img
-                src={`http://localhost:1337${movie.poster.url}`}
-                alt={`${movie.title} poster`}
-                className="movie-poster"
-              />
-            )}
-            <p className="movie-description">
-              {movie.description.substring(0, 200) + "..."}
-              <NavLink
-                to={"/movie"} //{`/movie/${movie.id}-${movie.title}`}
-                end
-                onClick={() => handleMovieClick(movie)}
-              >
-                Läs mer ➡️
-              </NavLink>
-            </p>
+        ) : (
+          searchFilter.map((movie) => (
+            <div key={movie.id} className="movie-card">
+              <h2>{movie.title}</h2>
+              {movie.poster && (
+                <img
+                  src={`http://localhost:1337${movie.poster.url}`}
+                  alt={`${movie.title} poster`}
+                  className="movie-poster"
+                />
+              )}
+              <p className="movie-description">
+                {movie.description.substring(0, 200) + "..."}
+                <NavLink
+                  to={"/movie"} //{`/movie/${movie.id}-${movie.title}`}
+                  end
+                  onClick={() => handleMovieClick(movie)}
+                >
+                  Läs mer ➡️
+                </NavLink>
+              </p>
+            </div>
+          ))
+        )}
+      </div>
+      {selectedMovie && (
+        <div className="movie-review-container">
+          <div className="movie-review">
+            Betyg: {selectedMovie.rating}
+            <br />
+            {selectedMovie.review}
           </div>
-        ))
+        </div>
       )}
     </div>
-  </div>
   );
 };
-
 export default Movie;
